796883712894dd01d97a60e015081a5516f66ca2
Twig Nielsen <littleb88888888@gmail.com>
2024-06-07 15:56:38 -0600

    Limited ripple spawn rate

--- main.cpp
+++ main.cpp
@@ -12,14 +12,27 @@ int main(void)
     InitWindow(screenSize.x, screenSize.y, "raylib cpp test");
     SetTargetFPS(60);
 
-    WavePool wavePool = WavePool(screenSize, 20, 32, 18, 8, LIME);
+    WavePool wavePool = WavePool(screenSize, 20, 48, 27, 6, LIME);
+
+    float ripple_spawn_delay = 1.0 / 30;
+    float ripple_spawn_delay_timer = 0;
 
     // Main Game Loop
     while(!WindowShouldClose())
     {
         auto mousePosition = GetMousePosition();
+
+        ripple_spawn_delay_timer += GetFrameTime();
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-            wavePool.AddRipple(Ripple(vec2(mousePosition), 5));
+        {
+            if (ripple_spawn_delay_timer >= ripple_spawn_delay)
+            {
+                ripple_spawn_delay_timer -= ripple_spawn_delay;
+                wavePool.AddRipple(Ripple(vec2(mousePosition), 10));
+            }
+        }
+        else if (ripple_spawn_delay_timer > ripple_spawn_delay)
+            ripple_spawn_delay_timer = ripple_spawn_delay;
         
         wavePool.Update();
 
