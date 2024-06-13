4a45872480c21f6b2b9e67bdcf5b7324a3388728
Twig Nielsen <littleb88888888@gmail.com>
2024-06-12 23:17:37 -0600

    Implemented settings struct - added settings struct to hold and handle user preferences - implemented saving and loading of settings to plaintext - added file utils to provide helper functions to settings saving and loading

---
+++ settings/settings.cpp
@@ -0,0 +1,104 @@
+#include "raylib.h"
+#include "../utils/file_utils.cpp"
+#include <cstring>
+
+namespace settings {
+  #define SAVE_PATH "settings.txt"
+
+  /* Settings object saved as unlabelled plaintext values in the format:
+  [volume]
+  [fullscreen]
+  [window width], [window height], [margin], 
+  [dot radius], [dot spacing]
+  [background colour], [guide colour], [main colour]
+  */
+  struct Settings
+  {
+    float volume = 1.0;
+
+    bool fullscreen = false;
+    float windowWidth = 800;
+    float windowHeight = 800;
+    float margin = 40;
+
+    float dotSize = 6;
+    float dotSpacing = 16;
+
+    Color backgroundColour = utils::GetColourFromString("96c9dc");
+    Color guideColour = utils::GetColourFromString("61a0af");
+    Color mainColour = utils::GetColourFromString("#f5d491");
+
+
+    void SaveToFile(const char* path = SAVE_PATH)
+    {
+      char* fileText = new char [1000];
+      *fileText = '\0';
+      utils::ConcatenateLine(fileText, volume);
+      utils::ConcatenateLine(fileText, fullscreen);
+
+      float windowValues [] = {windowWidth, windowHeight, margin};
+      utils::ConcatenateLine(fileText, windowValues);
+
+      float gridValues [] = {dotSize, dotSpacing};
+      utils::ConcatenateLine(fileText, gridValues);
+
+      Color colours [] = {backgroundColour, guideColour, mainColour};
+      utils::ConcatenateLine(fileText, colours);
+
+      SaveFileText(path, fileText);
+      delete fileText;
+    }
+
+    static Settings LoadFromFile(const char* path = SAVE_PATH)
+    {
+      if (FileExists(path))
+      {
+        char* fileText = LoadFileText(path);
+        std::stringstream fileTextStream(fileText);
+        std::string fileTextLine;
+
+        if (fileText != NULL)
+        {
+          Settings loadedSettings = Settings();
+          // volume
+          if (std::getline(fileTextStream, fileTextLine, '\n'))
+          {
+            loadedSettings.volume = std::stof(fileTextLine);
+          }
+          // fullscreen
+          if (std::getline(fileTextStream, fileTextLine, '\n'))
+          {
+            loadedSettings.fullscreen = std::stoi(fileTextLine);
+          }
+          // window
+          if (std::getline(fileTextStream, fileTextLine, '\n'))
+          {
+            auto windowStrings = utils::SplitString(fileTextLine);
+            loadedSettings.windowWidth = std::stof(windowStrings[0]);
+            loadedSettings.windowHeight = std::stof(windowStrings[1]);
+            loadedSettings.margin = std::stof(windowStrings[2]);
+          }
+          // dot size and spacing
+          if (std::getline(fileTextStream, fileTextLine, '\n'))
+          {
+            auto numberStrings = utils::SplitString(fileTextLine);
+            loadedSettings.dotSize = std::stof(numberStrings[0]);
+            loadedSettings.dotSpacing = std::stof(numberStrings[1]);
+          }
+          // colours
+          if (std::getline(fileTextStream, fileTextLine, '\n'))
+          {
+            auto colourStrings = utils::SplitString(fileTextLine);
+            loadedSettings.backgroundColour = utils::GetColourFromString(colourStrings[0]);
+            loadedSettings.guideColour = utils::GetColourFromString(colourStrings[1]);
+            loadedSettings.mainColour = utils::GetColourFromString(colourStrings[2]);
+          }
+          return loadedSettings;
+        }
+      }
+      Settings defaultSettings = Settings();
+      defaultSettings.SaveToFile(path);
+      return defaultSettings;
+    }
+  };
+}
---
+++ utils/file_utils.cpp
@@ -0,0 +1,84 @@
+#include "raylib.h"
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <vector>
+
+namespace utils {
+  Color GetColourFromString(std::string hexString)
+  {
+    if (hexString[0] == '#')
+      hexString.erase(0, 1);
+    unsigned int hexValue;
+    std::stringstream ss;
+    ss << std::hex << hexString;
+    ss >> hexValue;
+    return GetColor(hexValue);
+  }
+
+  const char* GetColourString(Color colour)
+  {
+    std::stringstream ss;
+    ss << std::hex << ColorToInt(colour);
+    return ss.str().c_str();
+  }
+
+  std::vector<std::string> SplitString(std::string s, std::string delimeter = ", ")
+  {
+    std::vector<std::string> result;
+    size_t pos = 0;
+    while((pos = s.find(delimeter)) != std::string::npos) {
+      result.push_back(s.substr(0, pos));
+      s.erase(0, pos + delimeter.length());
+    }
+    result.push_back(s);
+    return result;
+  }
+
+  void ConcatenateLine(char* s, const char* newLine)
+  {
+    strcat(s, newLine);
+    strcat(s, "\n");
+  }
+
+  template <class T>
+  void ConcatenateLine(char* s, T value)
+  {
+    strcat(s, std::to_string(value).c_str());
+    strcat(s, "\n");
+  }
+
+  void ConcatenateLine(char* s, bool value)
+  {
+    strcat(s, value? "1": "0");
+    strcat(s, "\n");
+  }
+
+  template <size_t N>
+  void ConcatenateLine(char* s, Color (&colours)[N], const char* delimeter = ", ")
+  {
+    if (N == 0)
+      return;
+    strcat(s, GetColourString(colours[0]));
+    for (int i = 1; i < N; i++)
+    {
+      strcat(s, delimeter);
+      strcat(s, GetColourString(colours[i]));
+    }
+    strcat(s, "\n");
+  }
+
+  template <size_t N, class T>
+  void ConcatenateLine(char* s, T (&values)[N], const char* delimeter = ", ")
+  {
+    if (N == 0)
+      return;
+    std::string joinedString = std::to_string(values[0]);
+    for (int i = 1; i < N; i++)
+    {
+      joinedString += delimeter;
+      joinedString += std::to_string(values[i]);
+    }
+    ConcatenateLine(s, joinedString.c_str());
+  }
+}
