9bb89f8b106b942e5e7e39f7a7e36f31ad12cb9e
Twig Nielsen <littleb88888888@gmail.com>
2024-06-21 11:41:35 -0600

    Made web application fill available space

--- application_wrappers/web_application.cpp
+++ application_wrappers/web_application.cpp
@@ -1,12 +1,28 @@
 #include "wave_pool_application.cpp"
+#include <emscripten.h>
 
 namespace application_wrappers {
+  EM_JS(int, GetDocumentWidth, (), {
+    return document.body.clientWidth;
+  });
+
+  EM_JS(int, GetDocumentHeight, (), {
+    return document.body.clientHeight;
+  });
+
   class WebApplication: public WavePoolApplication
   {
     public:
       void InitializeApplication()
       {
-        WavePoolApplication::InitializeApplication(vec2(300, 500), 30, 0.5);
+        int width = GetDocumentWidth();
+        int height = GetDocumentHeight();
+        WavePoolApplication::InitializeApplication(vec2(width, height), 30, 0.5);
+      }
+
+      void Update()
+      {
+        WavePoolApplication::Update();
       }
   };
 }
